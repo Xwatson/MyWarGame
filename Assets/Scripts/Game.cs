9bbe20274f9ab32d430566e7d741c99fa0e21057
Xwatson <xuwusheng@hotmail.com>
2018-03-28 00:00:53 +0800

    init project

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    private AudioManager audioManager;
+    private CameraManager cameraManager;
+    private ClientManager clientManager;
+    private PlayerManager playerManager;
+    private RequestManager requestManager;
+    private UIManager uIManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnDestroy()
+    {
+        DestroyManager();
+    }
+    private void InitManager()
+    {
+        audioManager = new AudioManager();
+        cameraManager = new CameraManager();
+        clientManager = new ClientManager();
+        playerManager = new PlayerManager();
+        requestManager = new RequestManager();
+        uIManager = new UIManager();
+
+        audioManager.OnInit();
+        cameraManager.OnInit();
+        clientManager.OnInit();
+        playerManager.OnInit();
+        requestManager.OnInit();
+        uIManager.OnInit();
+    }
+    private void DestroyManager()
+    {
+        audioManager.OnDestroy();
+        cameraManager.OnDestroy();
+        clientManager.OnDestroy();
+        playerManager.OnDestroy();
+        requestManager.OnDestroy();
+        uIManager.OnDestroy();
+    }
+}
---
+++ Assets/Scripts/Manager/ClientManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net.Sockets;
+
+public class ClientManager : BaseManager {
+
+    private const string HOST = "127.0.0.1";
+    private const int PORT = 3318;
+
+    private Socket clientSocket;
+
+    public override void OnInit()
+    {
+        base.OnInit();
+        try
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(HOST, PORT);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("连接服务器错误，请检查网络！" + e);
+        }
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        try
+        {
+            clientSocket.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("客户端关闭失败！" + e);
+        }
+    }
+}
